626e94bb38374c003cd58deab318f7cf1e25d096
Douglas Maia <douglasmaianf@gmail.com>
2023-11-04 19:28:39 -0300

    add estilização do footer

--- Shoptime/src/components/Footer.jsx
+++ Shoptime/src/components/Footer.jsx
@@ -1,15 +1,51 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-  return(
-    <>
+  const socialIconStyle = {
+    width: "30px",
+    marginRight: "10px",
+    marginBottom: "10px",
+  };
+
+  return (
+    <div style={{ backgroundColor: 'turquoise', padding: "30px" }}>
       <Container>
-        <div style={{display:'flex', backgroundColor:'turquoise'}}>
-          <h1>footer</h1>
-        </div>
+        <Row>
+          <Col md={3}>
+            <h4>Redes Sociais</h4>
+            <div>
+              <a href="https://www.instagram.com/" target="_blank" rel="">
+                <img style={socialIconStyle} src="https://seeklogo.com/images/I/instagram-logo-A807AD378B-seeklogo.com.png" alt="Instagram" />
+              </a>
+              <a href="https://www.facebook.com/" target="_blank" rel="">
+                <img style={socialIconStyle} src="https://www.svgrepo.com/show/3885/facebook.svg" alt="Facebook" />
+              </a>
+              <a href="https://github.com/tiktoneli/projeto-Final-Web-Dev-Grupo-3" target="_blank" rel="">
+                <img style={{ ...socialIconStyle, width: "30px" }} src="https://www.logo.wine/a/logo/GitHub/GitHub-Wordmark-Logo.wine.svg" alt="GitHub" />
+              </a>
+            </div>
+          </Col>
+          <Col md={6}>
+            <h4>Contato</h4>
+            <p>Telefone: (123) 456-7890</p>
+            <p>E-mail: info@shoptime.com</p>
+            <p>Endereço: 1234 Elm Street, Cidade FriburgoTerê</p>
+          </Col>
+          <Col md={3}>
+            <h4>Loja Shoptime</h4>
+            <a href="/sobre-nos" target="_blank" rel="">
+              Sobre Nós
+            </a>
+            <br />
+            <a href="/politicas-de-privacidade" target="_blank" rel="">
+              Políticas de Privacidade
+            </a>
+          </Col>
+        </Row>
       </Container>
-    </>
+    </div>
   )
 }
-export default Footer
+
+export default Footer;
