6a54b4d6a4b88786ce3e1962b818124addef88f6
douglasmariano <douglas.arm@live.com>
2023-11-06 17:55:02 -0300

    acertando o style do historico de pedido

--- Shoptime/src/pages/historicoPedidos/HistoricoPedidos.jsx
+++ Shoptime/src/pages/historicoPedidos/HistoricoPedidos.jsx
@@ -1,32 +1,29 @@
-import React, { useEffect, useContext } from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useEffect, useContext } from "react";
+import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CardPedidos from '../../components/CardPedidos';
-import { LojaContext } from '../../context/LojaContext';
-import { useNavigate } from 'react-router-dom';
+import CardPedidos from "../../components/CardPedidos";
+import { LojaContext } from "../../context/LojaContext";
+import { useNavigate } from "react-router-dom";
 
 const HistoricoPedidos = () => {
-
-  const { pedidos, usuarioLogado } = useContext(LojaContext)
-  const navigate = useNavigate()
+  const { pedidos, usuarioLogado } = useContext(LojaContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (!usuarioLogado.nome){
-      alert('Faça o login para ver seus pedidos feitos!')
-      navigate('/home')
+    if (!usuarioLogado.nome) {
+      alert("Faça o login para ver seus pedidos feitos!");
+      navigate("/home");
     }
 
-    fetchPedidos()
+    fetchPedidos();
   }, []);
 
-  const fetchPedidos = () => {
-    
-  }
+  const fetchPedidos = () => {};
   return (
     <Container fluid className="p-0 vh-100 d-flex flex-column bg-light">
       <Container>
-        <h3>{usuarioLogado.nome}: Histórico de Pedidos</h3>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+        <h3>Histórico de Pedidos</h3>
+        <div style={{ display: "flex", maxWidth: "1440px", flexWrap: "wrap" }}>
           {pedidos.map((pedido, index) => (
             <CardPedidos pedido={pedido} />
           ))}
